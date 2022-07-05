831399165d36342dd40257b69dfbb5e936700d77
MR.TIMA <98197960+TEAM-KING-BOT@users.noreply.github.com>
2022-07-05 05:47:21 +0100

    Create king-bot-start.sh

---
+++ king-bot-start.sh
@@ -0,0 +1,12 @@
+. /root/manoj-multi-device-whatsapp-pre-data.sh
+
+printf "\033c"
+# Main Display
+echo "${echo_Main_wa_Display}"
+# Show Logs
+echo "
+# Manoj Multi-device Whatsapp Bot
+#
+#   - Not yet published
+#
+"
