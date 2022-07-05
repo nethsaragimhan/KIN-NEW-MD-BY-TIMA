ca3f0a4adb62113182cfbd372407b84ae1c8b5f7
MR.TIMA <98197960+TEAM-KING-BOT@users.noreply.github.com>
2022-07-05 05:52:09 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-return
-const bash = require("@ravindu01manoj/bash-js")
-const command = "node manoj.js"
-const startFileWithNode = require("../indexstart.js")
-
-async function start() {
-	return await startFileWithNode(bash,command)
-}
-start()
